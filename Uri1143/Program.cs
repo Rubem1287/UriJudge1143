cb8e7447196fb80ff5bb13327bf21a5787bf1031
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 16:26:11 -0300

    Add files via upload

---
+++ Uri1143/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Uri1143
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            double a, b, c;
+            a = 1;
+            b = 1;
+            c = 1;
+
+            n = int.Parse(Console.ReadLine());
+
+            while(n>1000)
+            {
+                n = int.Parse(Console.ReadLine());
+            }
+
+            for (double i = 1; i <= n; i++)
+            {
+                
+                a = i;
+                b = Math.Pow(i, 2);
+                c = Math.Pow(i,3);
+                Console.WriteLine(a + " " + b + " " + c);
+            }
+        }
+    }
+}
